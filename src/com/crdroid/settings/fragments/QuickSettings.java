58d55dcd44fc619d7ada644ed5972761d8f8ad80
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-16 08:37:28 +0000

    Settings: Implement Better QS [3/3]
    Change-Id: I9d71d7de25034cf434cf3aaf180e40f3fadf7398

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -18,6 +18,7 @@ package com.crdroid.settings.fragments;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 
@@ -62,8 +63,13 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private SwitchPreferenceCompat mShowAutoBrightness;
     private ListPreference mQsUI;
     private ListPreference mQsPanelStyle;
+    private Preference mSplitShadePref;    
 
     private static ThemeUtils mThemeUtils;
+
+    private  ThemeUtils mThemeUtils_split;
+
+    private Handler mHandler = new Handler();    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -104,6 +110,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mQsPanelStyle.setOnPreferenceChangeListener(this);
 
         checkQSOverlays(mContext);
+
+        mSplitShadePref = (Preference) findPreference("qs_split_shade_enabled");
+        mSplitShadePref.setOnPreferenceChangeListener(this);        
     }
 
     @Override
@@ -131,6 +140,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             updateQsPanelStyle(getContext());
             checkQSOverlays(getContext());
             return true;
+        } else if (preference == mSplitShadePref) {
+            int value = (boolean) newValue ? 1 : 0;
+            Settings.System.putIntForUser(resolver,
+                "qs_split_shade_enabled", value, UserHandle.USER_CURRENT);
+            updateSplitShadeEnabled(getActivity());
+            return true;            
         }
         return false;
     }
@@ -286,6 +301,25 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         index = mQsPanelStyle.findIndexOfValue(Integer.toString(qsPanelStyle));
         mQsPanelStyle.setValue(Integer.toString(qsPanelStyle));
         mQsPanelStyle.setSummary(mQsPanelStyle.getEntries()[index]);
+    }
+
+    private void updateSplitShadeEnabled(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        boolean splitShadeEnabled = Settings.System.getIntForUser(
+                resolver,
+                "qs_split_shade_enabled" , 0, UserHandle.USER_CURRENT) != 0;
+	    String splitShadeStyleCategory = "android.theme.customization.better_qs";
+        String overlayThemeTarget  = "com.android.systemui";
+        String overlayThemePackage  = "com.android.system.qs.ui.better_qs";
+        if (mThemeUtils_split == null) {
+            mThemeUtils_split = new ThemeUtils(context);
+        }
+        mHandler.postDelayed(() -> {
+            mThemeUtils_split.setOverlayEnabled(splitShadeStyleCategory, overlayThemeTarget, overlayThemeTarget);
+            if (splitShadeEnabled) {
+                mThemeUtils_split.setOverlayEnabled(splitShadeStyleCategory, overlayThemePackage, overlayThemeTarget);
+            }
+        }, 1250);
     }
 
     @Override
